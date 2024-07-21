678104a71804c6842f137ab4a5e08367636fb1e8
Артем <timforworking@mail.ru>
2024-07-21 23:28:50 +0300

    fix

--- back/maps_app/views.py
+++ back/maps_app/views.py
@@ -486,6 +486,15 @@ class MapLayerViewSet(ModelViewSet):
         serializer = self.get_serializer(maps, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user.is_manager:
+            if not has_company_access(request.user, instance.company):
+                return Response({"detail": "У вас нет доступа к этому слою."}, status=status.HTTP_403_FORBIDDEN)
+
+        self.perform_destroy(instance)
+        return Response({"message": "Как же я устал"}, status=status.HTTP_200_OK)
+
 
 class MapStyleViewSet(ModelViewSet):
     queryset = MapStyle.objects.all()
