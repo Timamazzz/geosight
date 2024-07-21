d6f6adab164d8270dfeb8450922ea1ee75c0edd4
Артем <timforworking@mail.ru>
2024-07-22 00:05:59 +0300

    fix

--- back/users_app/views.py
+++ back/users_app/views.py
@@ -261,6 +261,14 @@ class UserViewSet(ModelViewSet):
         serializer = UserCardSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user.is_manager:
+            if request.user.company != instance.company:
+                return Response({"detail": "У вас нет доступа."}, status=status.HTTP_403_FORBIDDEN)
+
+        self.perform_destroy(instance)
+        return Response({"message": "Как же я устал"}, status=status.HTTP_200_OK)
 
 class CompanyViewSet(ModelViewSet):
     queryset = Company.objects.all()
