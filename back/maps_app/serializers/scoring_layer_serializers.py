269d95f53d4f94aa4273f87c1dd6be3c06fc06d8
Артем <timforworking@mail.ru>
2024-07-22 21:05:32 +0300

    fix

--- back/maps_app/serializers/scoring_layer_serializers.py
+++ back/maps_app/serializers/scoring_layer_serializers.py
@@ -11,17 +11,11 @@ class ScoringLayerSerializer(serializers.ModelSerializer):
 
 class ScoringLayerListSerializer(serializers.ModelSerializer):
     maps = serializers.SerializerMethodField()
-    companies = serializers.SerializerMethodField()
 
     class Meta:
         model = CreateScoringMapLayerTask
-        fields = ('id', 'task_id', 'layer', 'maps', 'status', 'created_at', 'end_time', 'companies')
+        fields = ('id', 'task_id', 'layer', 'maps', 'status', 'created_at', 'end_time')
 
     def get_map(self, obj):
         maps = obj.layer.maps.all()
         return [map.name for map in maps]
-
-    def get_companies(self, obj):
-        request = self.context.get('request')
-        companies = obj.layer.maps.filter(company=request.user.company).distinct()
-        return [company.pk for company in companies]
--- back/maps_app/views.py
+++ back/maps_app/views.py
@@ -523,7 +523,7 @@ class MapLayerViewSet(ModelViewSet):
 
         page = self.paginate_queryset(queryset)
         if page is not None:
-            serializer = self.get_serializer(page, many=True, context={'request': request})
+            serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
     @action(methods=['post'], detail=False)
