7ae7916a0a5ad069e17ec8f80e802452928b7462
Артем <timforworking@mail.ru>
2024-07-28 17:52:40 +0300

    fix

--- back/users_app/views.py
+++ back/users_app/views.py
@@ -291,3 +291,8 @@ class CompanyViewSet(ModelViewSet):
     }
     permission_classes = [IsAdmin]
     search_fields = ('id', 'name')
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": "Как же я устал"}, status=status.HTTP_200_OK)
